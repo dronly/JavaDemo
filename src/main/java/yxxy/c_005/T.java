a1c3de252d1ec88696a20f16895c20e21fc49eb2
boboo <ma_yanboo@163.com>
2020-03-03 17:48:47 +0800

    马士兵老师java多线程高并发编程
    https://www.bilibili.com/video/av33688545/

---
+++ src/main/java/yxxy/c_005/T.java
@@ -0,0 +1,27 @@
+package yxxy.c_005;
+
+
+import java.util.concurrent.TimeUnit;
+
+class T implements Runnable {
+
+    private int count=10;
+
+    @Override
+    public /*synchronized */ void run() {
+        count--;
+        try {
+            TimeUnit.MILLISECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName()+" count = "+count);
+    }
+
+    public static void main(String[] args) {
+        T t=new T();
+        for (int i = 0; i < 5; i++) {
+            new Thread(t,"Thread"+i).start();
+        }
+    }
+}
